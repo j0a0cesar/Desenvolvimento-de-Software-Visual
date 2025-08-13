d21c47a7d28b160d914d64db59d9a7ff986ca788
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-13 17:11:14 -0300

    Create ex4.cs

---
+++ aula02/ex4.cs
@@ -0,0 +1,19 @@
+/*Exercício 4 – Tipos booleanos
+Crie uma variável chamada temCarteiraMotorista do tipo bool. Ela deve ser true. 
+Depois, exiba no console a mensagem: "Você pode dirigir? true"
+*/
+
+namespace ExerciosCSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool temCarteiraMotorista = true;
+            Console.WriteLine("Você tem carteira de motorista? " + temCarteiraMotorista);   
+
+
+            
+        }
+    }
+}
