147cf5dfd59035989b578a85ca2ad73756edfa6e
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-08-20 20:14:12 -0300

    Create web.cs

---
+++ aula 4/web.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build(); // vai subir a sua aplicação
+
+app.MapGet("/", () => "Hello World!"); // o MapGet é metodp get 
+
+
+() => {}
+//funçao anonima
+// higher order function -> função de alta ordem
+// lambda
+
+
+MapGet("xablau", () => {
+
+})
+
+app.Run(); // vai pegar todos metodos e vai pegar e rodar a aplicação
